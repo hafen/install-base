b290056dc1207013b8c10cd8f8d431c3d2e63664
hafen <rhafen@gmail.com>
2014-12-05 15:32:37 -0800

    Add dependencies.sh

--- cdh5mr2precise/dependencies.sh
+++ cdh5mr2precise/dependencies.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-apt-get update -q -q
-apt-get dist-upgrade --yes --force-yes
+sudo apt-get update -q -q
+sudo apt-get dist-upgrade --yes --force-yes
 
-apt-get update -q -q
-apt-get install -y sudo vim wget curl git ant maven
+sudo apt-get update -q -q
+sudo apt-get install -y sudo vim wget curl git ant maven
