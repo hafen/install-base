0af1672b3609d350afb92af41787d466ae775d1e
hafen <rhafen@gmail.com>
2014-12-06 11:12:12 -0800

    Update

--- cdh5mr2precise/tessera.sh
+++ cdh5mr2precise/tessera.sh
@@ -1,37 +1,45 @@
 #!/bin/bash
 
-cd /tmp
-## protobuf
-wget https://protobuf.googlecode.com/files/protobuf-$PROTO_BUF_VERSION.tar.bz2
-tar jxvf protobuf-$PROTO_BUF_VERSION.tar.bz2
-cd protobuf-$PROTO_BUF_VERSION
-./configure && make -j4
-make install
-cd ..
-rm -rf protobuf-*
+export PROTO_BUF_VERSION=2.5.0
+export RHIPE_VERSION=Rhipe_0.75.0_cdh5mr2
 
-## RHIPE
-USER tessera
+if [ -f ~/.build_tessera_done ]
+then
+   echo "Skipping Tessera components installation - already done"
+else
+   cd /tmp
 
-export LD_LIBRARY_PATH=/usr/local/lib:$LD_LIBRARY_PATH \
-export PKG_CONFIG_PATH=/usr/local/lib \
-wget http://ml.stat.purdue.edu/rhipebin/$RHIPE_VERSION.tar.gz \
-R CMD INSTALL $RHIPE_VERSION.tar.gz
-rm $RHIPE_VERSION.tar.gz \
-echo "export LD_LIBRARY_PATH=/usr/local/lib" | tee -a /home/tessera/rhRunner.sh \
-echo "exec /usr/bin/R CMD /usr/local/lib/R/site-library/Rhipe/bin/RhipeMapReduce --slave --silent --vanilla" | tee -a /home/tessera/rhRunner.sh \
-chown -R tessera:tessera /home/tessera \
-chmod 755 /home/tessera \
-chmod 755 /home/tessera/rhRunner.sh
+   ## protobuf
+   wget https://protobuf.googlecode.com/files/protobuf-$PROTO_BUF_VERSION.tar.bz2
+   tar jxvf protobuf-$PROTO_BUF_VERSION.tar.bz2
+   cd protobuf-$PROTO_BUF_VERSION
+   ./configure && make -j4
+   sudo make install
+   cd ..
+   rm -rf protobuf-*
 
-## do initial downloading of maven artifacts
-cd /home/tessera
-git clone --depth=50 --recursive --branch=dev git://github.com/hafen/RHIPE.git
-cd RHIPE
-rm -rf /home/tessera/.m2
-mvn package --fail-never
-cd ..
-rm -rf RHIPE
+   ## RHIPE
+   export LD_LIBRARY_PATH=/usr/local/lib:$LD_LIBRARY_PATH \
+   export PKG_CONFIG_PATH=/usr/local/lib \
+   wget http://ml.stat.purdue.edu/rhipebin/$RHIPE_VERSION.tar.gz \
+   R CMD INSTALL $RHIPE_VERSION.tar.gz
+   rm $RHIPE_VERSION.tar.gz \
+   echo "export LD_LIBRARY_PATH=/usr/local/lib" | tee -a /home/$TESSERA_USER/rhRunner.sh \
+   echo "exec /usr/bin/R CMD /usr/local/lib/R/site-library/Rhipe/bin/RhipeMapReduce --slave --silent --vanilla" | tee -a /home/$TESSERA_USER/rhRunner.sh \
+   chown -R $TESSERA_USER:$TESSERA_USER /home/$TESSERA_USER \
+   chmod 755 /home/$TESSERA_USER \
+   chmod 755 /home/$TESSERA_USER/rhRunner.sh
 
-sudo -u $TESSERA_USER R -e "options(unzip = 'unzip', repos = 'http://cran.rstudio.com/'); library(devtools); install_github('tesseradata/datadr')"
-sudo -u $TESSERA_USER R -e "options(unzip = 'unzip', repos = 'http://cran.rstudio.com/'); library(devtools); install_github('tesseradata/trelliscope')"
+   ## do initial downloading of maven artifacts
+   git clone --depth=50 --recursive --branch=dev git://github.com/hafen/RHIPE.git
+   cd RHIPE
+   rm -rf /home/$TESSERA_USER/.m2
+   mvn package --fail-never
+   cd ..
+   rm -rf RHIPE
+
+   sudo -u $TESSERA_USER R -e "options(unzip = 'unzip', repos = 'http://cran.rstudio.com/'); library(devtools); install_github('tesseradata/datadr')"
+   sudo -u $TESSERA_USER R -e "options(unzip = 'unzip', repos = 'http://cran.rstudio.com/'); library(devtools); install_github('tesseradata/trelliscope')"
+
+   touch ~/.build_tessera_done
+fi
