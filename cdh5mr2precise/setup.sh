9d5a9d6ced8778160ce912c5f305c7251a4e767c
hafen <rhafen@gmail.com>
2014-12-05 18:31:11 -0800

    Update

--- cdh5mr2precise/setup.sh
+++ cdh5mr2precise/setup.sh
@@ -4,13 +4,13 @@
 export TESSERA_USER=${TESSERA_USER:-tessera}
 
 # misc dependencies
-if [[ $BUILD_SETUP_DONE && ${BUILD_SETUP_DONE-_} ]]
+if [ -f ~/.build_setup_done ]
 then
    echo "Skipping setup - already done"
 else
    sudo apt-get update -q -q
    sudo apt-get install -y sudo vim wget curl git ant maven
    echo "Setup finished..."
-   export BUILD_SETUP_DONE=1
+   touch ~/.build_setup_done
 fi
 
