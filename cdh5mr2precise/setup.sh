e1c53b738f01e82992bd87edfeab580d59771219
hafen <rhafen@gmail.com>
2014-12-05 16:21:18 -0800

    Add setup.sh

---
+++ cdh5mr2precise/setup.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# if not specified, tessera user is tessera
+export $TESSERA_USER=${TESSERA_USER:=tessera}
+
+# misc dependencies
+sudo apt-get update -q -q
+sudo apt-get install -y sudo vim wget curl git ant maven
