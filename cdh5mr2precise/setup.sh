fdfeff2832bbc04f02e99b2befcfde86e95ecfc7
hafen <rhafen@gmail.com>
2014-12-05 17:22:08 -0800

    Update setup.sh

--- cdh5mr2precise/setup.sh
+++ cdh5mr2precise/setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # if not specified, tessera user is tessera
-export $TESSERA_USER=${TESSERA_USER:=tessera}
+export TESSERA_USER=${TESSERA_USER:-tessera}
 
 # misc dependencies
 sudo apt-get update -q -q
